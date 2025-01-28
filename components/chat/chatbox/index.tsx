bc4556167d768315e087b4bcdc06be085ba8b14f
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-01-28 08:58:41 +0700

    First Init

---
+++ components/chat/chatbox/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div className='w-full border border-gray-300 h-screen'>
+      <div className='w-full h-10 bg-blue-600 text-white px-2 py-2'>
+        <p className='font-bold'>UserName</p>
+      </div>
+      <div className='px-2 py-2'>
+        Chatbox
+      </div>
+    </div>
+  )
+}
