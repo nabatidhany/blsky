eb617661a8691bd5c0cd26ed0f7771ae9d4d66d3
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-01-28 09:46:12 +0700

    FEAT: Customize Chatbox and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Chatbox from '../components/chat/chatbox';
 export default function Home() {
   return (
     <>
-      <div className='flex justify-center items-center h-screen gap-2'>
-        <Chatbox />
-        <Chatbox />
+      <div className='flex justify-center items-center h-screen gap-6 px-24 py-16'>
+        <Chatbox id='left' />
+        <Chatbox id='right' />
       </div>
     </>
   );
--- components/chat/chatbox/index.tsx
+++ components/chat/chatbox/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
-export default function Index() {
+export default function Index({id}: {id: string}) {
+  const [chats, setChats] = useState([
+    {id: 1, message: 'Hello', sender: 'left'},
+    {id: 2, message: 'Hai', sender: 'right'},
+  ])
+  const [message, setMessage] = useState('')
+  const handleSend = () => {
+    setChats([...chats, {id: chats.length + 1, message: message, sender: id}]);
+    setMessage('')
+  }
   return (
-    <div className='w-full border border-gray-300 h-screen'>
-      <div className='w-full h-10 bg-blue-600 text-white px-2 py-2'>
-        <p className='font-bold'>UserName</p>
+    <div className='w-full border border-gray-300 h-full relative rounded-md overflow-hidden shadow-md'>
+      <div className='px-2 py-2 h-screen overflow-y-scroll pb-56'>
+        {
+          chats.map((chat) => (
+            <div key={chat.id} className={`flex gap-2 ${chat.sender === id ? 'justify-end' : 'justify-start'}`}>
+              <div className={`px-2 py-2 rounded-md ${chat.sender === id ? 'bg-gray-200' : 'bg-blue-600 text-white'}`}>
+                {chat.message}
+              </div>
+            </div>
+          ))
+        }
       </div>
-      <div className='px-2 py-2'>
-        Chatbox
+      <div className='absolute bottom-0 w-full bg-white px-2 py-2 shadow-md flex justify-between items-center gap-2'>
+        <input
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSend()
+            }
+          }} 
+          onChange={(e) => setMessage(e.target.value)} value={message} type='text' className='w-full border border-gray-300 px-2 py-2 rounded-sm' />
+        <button onClick={() => handleSend()} className='bg-blue-600 text-white px-2 py-2 rounded-sm'>Send</button>
       </div>
     </div>
   )
